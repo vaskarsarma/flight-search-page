9148b43d1fee35229cbf47fd3cb867f82f499315
Vaskar Sarma <vaskar.sarma@emirates.com>
2021-09-25 22:55:53 +0400

    Remove unused components

--- src/components/flightlogo/flightlogo.js
+++
@@ -1,9 +0,0 @@
-import "./flightlogo.scss";
-
-const FlightLogo = (props) => {
-  return (
-    <img src={props.imagePath} className={props.size} alt={props.alttext}></img>
-  );
-};
-
-export default FlightLogo;
--- src/components/footer/footer.js
+++
@@ -1,7 +0,0 @@
-import "./footer.scss";
-
-const Footer = () => {
-  return <footer className="App-footer"></footer>;
-};
-
-export default Footer;
